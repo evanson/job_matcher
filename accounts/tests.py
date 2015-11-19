e043475f12e0f2b481a7a4c440359a290aea56de
Evanson Wachira <evansonwachira@gmail.com>
2015-11-19 20:04:46 +0300

    Update signup test

--- accounts/tests.py
+++ accounts/tests.py
@@ -3,8 +3,66 @@ from django.test.utils import override_settings
 from django.core.urlresolvers import reverse
 from django.core import mail
 
+from django.core.exceptions import ObjectDoesNotExist
+from job_skills.models import JobSkill
+
 
 class TestJobSeekerSignup(LiveServerTestCase):
+    def setUp(self):
+        skills = {'accounting': ['Book Keeping', 'Balance Sheets', 'P & L', 'Petty Cash Management', 'Taxation',  'Cost Management', 'Budgeting'],
+                  
+                  'administration': ['Data Entry', 'Virtual Assistance', 'Research', 'Transcription', 'Secretarial'],
+                  
+                  'agriculture': ['Vertinary', 'Crop Production', 'Large Scale Farming', 'Animal Husbandry'],
+                  
+                  'audit': ['Auditing', 'Risk Analysis', ],
+                  
+                  'banking': ['Customer Acquisition', 'Credit Management', 'Corporate Banking'],
+                  
+                  'business_administration': ['Business Plans', 'Corporate Governance'],
+                  
+                  'communication': ['Public Relations', 'Media Relations'],
+                  
+                  'community_development': ['Humanitarian work'],
+                  
+                  'customer_service': ['Call center management', 'Customer Relationship Management'],
+                  
+                  'design': ['Logos', 'Graphic Design', 'Illustration', 'Brochures', 'Banner Ads', 'Videos', 'Corporate Identity', 'Adobe Illustrator', 'Adobe Photoshop',
+                             '3D Modelling', '3D Animation', '2D Animation', '2D Modelling'],
+                  
+                  'education': ['ECD'],
+                  
+                  'engineering': ['CAD', 'Electrical', 'Architecture', 'Mechanical', 'Product Design', 'Civil', 'Structural', 'Contract Manufacturing'],
+                  
+                  'health': ['General Practitioning', 'Nursing', 'Public Health'],
+                  
+                  'hospitality': ['Restaurant Management', 'Outside Catering', 'Tourism Operations'],
+                  
+
+                  'IT': ['ASP.NET', 'C#', 'MySQL', 'Postgres', 'MsSQL', 'C#', 'VB.NET', 'C', 'C++', 'Windows Administration', 'Linux Administration', 'iOS development',
+                         'Windows Phone Development', 'Android Development', 'Python', 'Ruby', 'Java', 'Javascript', 'Node JS', 'Angular JS', 'Ruby on Rails', 'Django',
+                         'Oracle', 'PHP', 'CSS', 'HTML', 'Networking & Security', 'CCNA', 'WordPress', 'Shopify', 'Joomla', 'Erlang', 'Golang', 'Delphi', 'Clojure',
+                         'Scala', 'Haskell', 'Adobe Acrobat', 'MongoDB', 'Cassandra', 'CouchDB', 'Riak', 'Perl', 'Ocaml', 'Lisp', 'Rust', 'VOIP', 'Web Scraping', 'Laravel',
+                         'Zend Framework', 'CodeIgniter', 'CakePHP', 'Symfony'],
+                  
+                  'legal': ['Contracts', 'Intellectual Property', 'Corporate', 'Real Estate', 'Wills, Trusts, Estates', 'Bankruptcy', 'Criminal Defence'],
+                  
+                  'media': ['TV Production', 'Radio Production', 'Video Production', 'Video Editing', 'Publishing', 'Editorial' ],
+                  
+                  'project_management': ['Cost Estimation', 'Scheduling', 'Staffing'],
+                  
+                  'sales_marketing': ['Ad campaigns', 'B2B Marketing', 'Corporate Sales', 'Email Marketing', 'Lead Generation', 'Marketing Strategy', 'Search Engine Marketing',
+                              'Telemarketing']}
+
+        for category, category_skills in skills.iteritems():
+            for skill in category_skills:
+                try:
+                    s = JobSkill.objects.get(category=category, name=skill)
+                except ObjectDoesNotExist:
+                    s = JobSkill(category=category, name=skill)
+                    s.save()
+
+        
     @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
                        CELERY_ALWAYS_EAGER=True,
                        BROKER_BACKEND='memory')
@@ -12,9 +70,9 @@ class TestJobSeekerSignup(LiveServerTestCase):
         url = reverse('job_seeker_signup', kwargs={'step': 'user'})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Job Seeker Sign Up')
+        self.assertContains(response, 'Sign Up')
 
-        data = {'jobseeker_wizard-current_step': 'user', 'user-username': 'evanson',
+        data = {'job_seeker_wizard-current_step': 'user', 'user-username': 'evanson',
                 'user-first_name': 'Evanson', 'user-last_name': 'Wachira',
                 'user-password1': 'evanson', 'user-password2': 'evanson',
                 'user-email': 'evansonwachira@gmail.com',
@@ -24,16 +82,21 @@ class TestJobSeekerSignup(LiveServerTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Job Experience Details")
 
-        profile_url = reverse('signup', kwargs={'step': 'job_seeker_profile'})
-        profile_data = {'jobseeker_wizard-current_step': 'job_seeker_profile',
-                        'category': 'IT', 'summary': 'Experienced software developer',
-                        'years_of_experience': 4, 'skills': ['34', '35', '37',
-                                                             '40', '44', '45',
-                                                             '47', '50']}
+        profile_url = reverse('job_seeker_signup', kwargs={'step': 'job_seeker_profile'})
+        profile_data = {'job_seeker_wizard-current_step': 'job_seeker_profile',
+                        'job_seeker_profile-category': 'IT',
+                        'job_seeker_profile-summary': 'Experienced software developer',
+                        'job_seeker_profile-years_of_experience': 4,
+                        'job_seeker_profile-skills': ['34', '35', '37',
+                                   '40', '44', '45',
+                                   '47', '50']}
         response = self.client.post(profile_url, data=profile_data,
                                     follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response, "You've successfully signed up.")
+        self.assertContains(response, "You've successfully signed up.")
 
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, 'Account Confirmation')
+
+        activation_link = mail.outbox[0].body.split('link ')[1]
+        print activation_link
